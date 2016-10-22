3b909af7da3c4fa68c5d297148064fa5e9fdc836
cata <gravedale02@gmail.com>
2016-10-22 21:15:45 +0300

    Exercitii

---
+++ src/Ex2.java
@@ -0,0 +1,14 @@
+/**
+ * Created by cata on 10/22/2016.
+ */
+public class Ex2 {
+    public static void main(String[] args) {
+        int n = 5;
+        int sum=0;
+        for (int i=0; i<n; i++){
+          sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+}
+
