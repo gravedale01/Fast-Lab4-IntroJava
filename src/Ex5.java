174cf39cdc2dd0eecccd96e6ffbaed6899c2887b
cata <gravedale02@gmail.com>
2016-10-23 17:46:29 +0300

    Exercitii

---
+++ src/Ex5.java
@@ -0,0 +1,14 @@
+/**
+ * Created by cata on 10/23/2016.
+ */
+public class Ex5 {
+    public static void main(String[] args) {
+        int a=3;
+        int n=5;
+        int prod=1;
+        for (int i=0; i<n; i++){
+        prod=prod*a;
+        }
+        System.out.println(prod);
+    }
+}
