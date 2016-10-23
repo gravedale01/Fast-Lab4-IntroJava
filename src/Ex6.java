ee05c40cb768fa41f4b1fdb9ee493c3725de149b
cata <gravedale02@gmail.com>
2016-10-23 18:04:51 +0300

    Exercitii

---
+++ src/Ex6.java
@@ -0,0 +1,13 @@
+/**
+ * Created by cata on 10/23/2016.
+ */
+public class Ex6 {
+    public static void main(String[] args) {
+        int a=5;
+        int prod=1;
+        for (int i=1; i<=a; i++){
+            prod=prod*i;
+        }
+        System.out.println(prod);
+    }
+}
